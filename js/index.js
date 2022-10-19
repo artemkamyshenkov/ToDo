4fa1167889ed6964bd8f86fd20c50c2a027aeb94
Artem <artmiller22831@gmail.com>
2022-10-19 20:59:11 +0300

    add task item for js

--- js/index.js
+++ js/index.js
@@ -48,3 +48,30 @@ function getLocalStorage() {
   }
 }
 window.addEventListener("load", getLocalStorage);
+
+const tasks = [
+  {
+    id: "1138465078061",
+    completed: false,
+    text: "Посмотреть новый урок по JavaScript",
+  },
+  {
+    id: "1138465078062",
+    completed: false,
+    text: "Выполнить тест после урока",
+  },
+  {
+    id: "1138465078063",
+    completed: false,
+    text: "Выполнить ДЗ после урока",
+  },
+];
+
+class Tasks {
+  createTask(taskId, taskText) {
+    const tasksContainer = document.querySelector(".tasks__container");
+    const taskItem = document.createElement("div");
+    taskItem.className = "task-item";
+    taskItem.dataset.taskId = taskId;
+  }
+}
